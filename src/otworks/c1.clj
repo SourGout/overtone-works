bf8831d4ae667e251b4f04b6189ea67621153483
SourGout <zach.whyhere@gmail.com>
2016-10-01 19:12:51 -0700

    small changes

--- src/otworks/c1.clj
+++ src/otworks/c1.clj
@@ -8,8 +8,6 @@
 (gen-inst "smplr" ["free-verb" "lpf" "warp1-lfo"])
 
 
-
-
 (definst warp
   [buf 0 point 1 fs 1 ws 10 ebn -1 overlaps 1 wrr 0.0 interp 1 amp 1 att 1 rel 1]
   (let [src
@@ -17,7 +15,6 @@
          (warp1 :bufnum buf :pointer point :freq-scale fs :window-size ws :envbufnum ebn :overlaps overlaps :window-rand-ratio wrr :interp interp)
          (env-gen (asr :attack att :curve 1 :release rel)))]
     (* src amp)))
-
 
 
 (definst warplfo
@@ -29,12 +26,14 @@
     (* src amp)))
 
 
-
-(def warpl1 (warplfo s3  :ws 10 :interp 4 :att 3 :rel 1))
+(def warpl1 (warplfo s3 :ws 10 :interp 4 :att 3 :rel 1))
 
 (def warpl2 (warplfo s2 :ws 5 :interp 4 :att 1 :rel 2))
 
-(ctl warpl2 :amp 1.5 :fs 2)
+(ctl warpl2 :amp 0 :fs 2)
+
+(ctl warpl1 :amp 0.5 :ws 20 :fs 2)
+
 
 (stop)
 
