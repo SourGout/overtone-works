e20af3ca6c6cfd642442ca933f90244ffebe6cc8
SourGout <zach.whyhere@gmail.com>
2016-10-02 22:32:41 -0700

    added gate to synthdefs

--- src/otworks/c1.clj
+++ src/otworks/c1.clj
@@ -9,34 +9,40 @@
 
 
 (definst warp
-  [buf 0 point 1 fs 1 ws 10 ebn -1 overlaps 1 wrr 0.0 interp 1 amp 1 att 1 rel 1]
+  [buf 0 point 1 fs 1 ws 10 ebn -1 overlaps 1 wrr 0.0 interp 1 amp 1 att 1 rel 1 gate 1]
   (let [src
         (*
          (warp1 :bufnum buf :pointer point :freq-scale fs :window-size ws :envbufnum ebn :overlaps overlaps :window-rand-ratio wrr :interp interp)
-         (env-gen (asr :attack att :curve 1 :release rel)))]
+         (env-gen (asr :attack att :curve 1 :release rel) :gate gate :action FREE))]
     (* src amp)))
 
 
 (definst warplfo
-  [buf 0 point 1 fs 1 ws 10 ebn -1 overlaps 1 wrr 0.0 interp 1 amp 1 att 1 rel 1]
+  [buf 0 point 1 fs 1 ws 10 ebn -1 overlaps 1 wrr 0.0 interp 1 amp 1 att 1 rel 1 gate 1]
   (let [src
         (*
          (warp1 :bufnum buf :pointer (+ 0.5 (* 0.1 (sin-osc:kr point))) :freq-scale fs :window-size ws :envbufnum ebn :overlaps overlaps :window-rand-ratio wrr :interp interp)
-         (env-gen (asr :attack att :curve 1 :release rel)))]
+         (env-gen (asr :attack att :curve 1 :release rel) :gate gate :action FREE))]
     (* src amp)))
 
 
-(def warpl1 (warplfo s1 :ws 1 :interp 4 :att 0 :rel 0 :wrr 4))
+(def warpl1 (warplfo s1 :ws 1 :interp 4 :att 0 :rel 5 :wrr 4))
 
 (def warpl2 (warplfo s1 :ws 4 :fs 2 :interp 4 :att 1 :rel 2 :wrr 1))
 
-(def warpl3 (warplfo s3 :ws 7 :interp 2 :att 0.6 :rel 5 :wrr 2))
+(def warpl3 (warplfo s1 :ws 7 :interp 2 :att 0.6 :rel 5 :wrr 2))
 
 (ctl warpl3 :amp 1.5 :fs 2)
 
-(ctl warpl1 :amp 1 :ws 20 :fs 2)
+(ctl warpl1 :amp 1 :ws 20 :fs 2 :freq 4)
 
 (ctl warpl2 :amp 2 :ws 10 :fs 1)
+
+(ctl warpl1 :gate 0)
+
+(ctl warpl2 :gate 0)
+
+(ctl warpl3 :gate 0)
 
 (stop)
 
